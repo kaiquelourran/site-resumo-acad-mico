d955dd92830a7f04252cdc5c9bb47c25cf110291
kique lourran silva nunes <kaiquenunis976@gmail.com>
2025-09-26 06:38:16 -0300

    Fix DEFINITIVO: URLs completas com localhost:8000 para corrigir redirecionamentos

--- questoes/listar_questoes.php
+++ questoes/listar_questoes.php
@@ -490,11 +490,11 @@ try {
                     
                     // Se há filtro ativo e não é "todas", vai para quiz sequencial
                     if (filtroAtivo && filtroAtivo !== 'todas') {
-                        url = `/questoes/quiz_sequencial.php?id=${idAssunto}&filtro=${filtroAtivo}&questao=${idQuestao}`;
+                        url = `http://localhost:8000/questoes/quiz_sequencial.php?id=${idAssunto}&filtro=${filtroAtivo}&questao=${idQuestao}`;
                         console.log('REDIRECIONANDO PARA QUIZ SEQUENCIAL:', url);
                     } else {
                         // Comportamento normal - vai para quiz individual
-                        url = `/questoes/quiz.php?id=${idQuestao}`;
+                        url = `http://localhost:8000/questoes/quiz.php?id=${idQuestao}`;
                         console.log('REDIRECIONANDO PARA QUIZ INDIVIDUAL:', url);
                     }
                     
